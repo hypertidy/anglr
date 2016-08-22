9c566f92bf2074e3bd034cddc52d62afec174e2f
Michael Sumner <mdsumner@gmail.com>
2016-08-22 21:00:01 +1000

    add test for polygons with hole code

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -4,12 +4,29 @@ context("basic")
 library(rworldxtra)
  data(countriesHigh)
 
+ a <- subset(countriesHigh, SOVEREIGNT == "Australia")
+
+ 
+
+
+library("spbabel")
+data("holey")
+test_that("holey polygons and non-holy polygons", {
+  expect_silent({
+    b <- tri_mesh(a)
+  })
+  expect_silent({
+    sph <- sp(holey)
+    tm <- tri_mesh(sph)
+  })
+  
+})
+
+
 test_that("simple plot", {
   expect_silent({
-    a <- subset(countriesHigh, SOVEREIGNT == "Australia")
     b <- tri_mesh(a)
-  #  options(rgl.useNULL=TRUE)
-   plot(b)
-
+    plot(b)
+    
   })
 })
