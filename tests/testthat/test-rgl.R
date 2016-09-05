5a8f3f125fa8bfc72270bd5b9150dd0d5cf50e9e
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-09-05 16:37:14 +1000

    rgl tests

---
+++ tests/testthat/test-rgl.R
@@ -0,0 +1,17 @@
+context("rgl")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+library(rgl)
+o <- oh3d()
+tt <- tetrahedron3d()
+p1 <- cbind(x = c(0, 0, 0.75, 1,   0.5, 0.8, 0.69, 0), 
+            y = c(0, 1, 1,    0.8, 0.7, 0.6, 0,    0))
+p <- extrude3d(p1)
+test_that("rgl objects can be ingested", {
+  expect_that(rangl(tt), is_a("trimesh"))
+  expect_that(rangl(p), gives_warning("quad"))
+  expect_that(rangl(o), throws_error("is not TRUE"))
+  
+})
