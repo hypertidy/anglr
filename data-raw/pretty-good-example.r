b58c1ed511d0b200699cedb40fd77fa4fc7942cb
Michael Sumner <mdsumner@gmail.com>
2016-09-07 20:32:49 +1000

    pretty good example

---
+++ data-raw/pretty-good-example.r
@@ -0,0 +1,19 @@
+## download the CCAMLR shapefiles . . .
+library(rangl)
+ssmu0 <- readRDS("data-raw/ssmumap.rds")
+#plot(ssmu[1:5,])
+idx <- 1:14
+ssmu <- rangl(ssmu0[idx, ], max_area = 5e6)
+print(ssmu$v)
+print(ssmu$tXv)
+print(all(spbabel::sptable(ssmu0[idx, ])$island_))
+library(raster)
+topo <- raster("C:/data/Etopo1/ETOPO1_Ice_c_gdal.grd")
+ll <- rgdal::project(as.matrix(ssmu$v[, c("x_", "y_")]), ssmu$meta$proj[1], inv = TRUE)
+ssmu$v$z <- extract(topo, ll, method = "bilinear")
+rm(ll, topo)
+gc();gc()
+
+ssmu$v$z_ <- ssmu$v$z * 10
+plot(globe(ssmu), specular = "black")
+library(rgl);bg3d("lightgrey")
