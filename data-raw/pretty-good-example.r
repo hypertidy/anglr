96c6a83d4a563bb91959bd183feaa7a45ac33c6c
Michael Sumner <mdsumner@gmail.com>
2016-09-12 17:32:07 +1000

    update pretty good example

--- data-raw/pretty-good-example.r
+++ data-raw/pretty-good-example.r
@@ -1,10 +1,18 @@
 ## download the CCAMLR shapefiles . . .
-library(rangl)
+library(sp)
 ssmu0 <- readRDS("data-raw/ssmumap.rds")
+
+
+library(rangl)
+spbabel::sptable(ssmu0)
+
+plot(rangl(ssmu0), specular = "black")
+
+
 #plot(ssmu[1:5,])
 idx <- 1:14
 ssmu <- rangl(ssmu0[idx, ], max_area = 5e6)
-print(ssmu$v)
+#print(ssmu$v)
 print(ssmu$tXv)
 print(all(spbabel::sptable(ssmu0[idx, ])$island_))
 library(raster)
@@ -15,5 +23,8 @@ rm(ll, topo)
 gc();gc()
 
 ssmu$v$z_ <- ssmu$v$z * 10
+plot(ssmu)
+
+
 plot(globe(ssmu), specular = "black")
 library(rgl);bg3d("lightgrey")
