0656ea77a8a62afa286f698a36b433b0e6b3d2c4
Michael Sumner <mdsumner@gmail.com>
2016-10-19 23:39:42 +1100

    carrots

--- docs/articles/rangl-examples.R
+++ docs/articles/rangl-examples.R
@@ -1,12 +1,19 @@
-## ---- echo = FALSE-------------------------------------------------------
+## ----setup, echo=FALSE, results="asis"-----------------------------------
+source("setup.R")
+knitr::opts_chunk$set(rgl.newwindow = TRUE)
+set.seed(123)
+
+## ---- echo = FALSE, message = FALSE--------------------------------------
 knitr::opts_chunk$set(
 collapse = TRUE,
 comment = "#>",
 fig.path = "vignettes/readme-figure/README-"
 )
 rgl::par3d(windowRect = c(100, 100, 512 + 100, 512 +100))
+library(raster)
+library(maptools)
 
-## ----eval=TRUE-----------------------------------------------------------
+## ----eval=TRUE, webgl=TRUE-----------------------------------------------
 
 library(rgl)
 library(maptools)
@@ -18,10 +25,7 @@ library(rangl)
 cmesh <- rangl(wrld_simpl)
 plot(cmesh)
 
-## snapshot code is only for this README
-rgl.snapshot("vignettes/readme-figure/README-wrld_simpl.png"); rgl.close()
-
-## ------------------------------------------------------------------------
+## ---- webgl=TRUE---------------------------------------------------------
 sids <- raster::shapefile(system.file("shapes/sids.shp", package="maptools"))
 projection(sids) <- "+proj=longlat +ellps=clrk66"
 
@@ -37,10 +41,7 @@ mgl <- rangl(gl)
 mgl$o$color_ <- "black"
 plot(globe(mgl), lwd = 2)
 
-rgl.snapshot("vignettes/readme-figure/README-sids-globe.png"); rgl.close()
-
-
-## ----eval=TRUE-----------------------------------------------------------
+## ----eval=TRUE, webgl=TRUE-----------------------------------------------
 library(spbabel)
 data(holey)
 
@@ -49,21 +50,16 @@ sph <- sp(holey)
 
 glh <- rangl(sph)
 plot(glh)
-rgl::rgl.snapshot("vignettes/readme-figure/README-home.png"); rgl.close()
 
-
-## ----eval=TRUE-----------------------------------------------------------
+## ---- webgl=TRUE---------------------------------------------------------
 linehouse <- as(sph, "SpatialLinesDataFrame")
 plot(rangl(linehouse))
-rgl::rgl.snapshot("vignettes/readme-figure/README-lines.png"); rgl.close()
 
-## ----eval=TRUE-----------------------------------------------------------
+## ---- webgl=TRUE---------------------------------------------------------
 lmesh <- rangl(as(wrld_simpl, "SpatialLinesDataFrame"))
 plot(globe(lmesh))
-rgl::rgl.snapshot("vignettes/readme-figure/README-liney-world.png"); rgl.close()
 
-
-## ----eval=TRUE-----------------------------------------------------------
+## ----webgl=TRUE----------------------------------------------------------
 library(rgl)
 
 dod <- rangl(dodecahedron3d(col = "cyan"))
@@ -71,10 +67,8 @@ octo <- rangl(translate3d(octahedron3d(col = "blue"), 6, 0, 0))
 plot(dod, col = viridis::viridis(5)[1], alpha = 0.3)
 plot(octo, col = viridis::viridis(5)[5], alpha = 0.3)
 bg3d("grey")
-rgl::rgl.snapshot("vignettes/readme-figure/README-Platonic.png"); rgl.close()
 
-
-## ------------------------------------------------------------------------
+## ---- webgl=TRUE---------------------------------------------------------
 library(rangl)
 library(maptools)
 data(wrld_simpl)
@@ -82,8 +76,32 @@ mpts <- as(as(wrld_simpl, "SpatialLinesDataFrame"), "SpatialMultiPointsDataFrame
 plot(rangl(mpts))
 rgl::view3d(theta = 25, phi = 3)
 
+## ---- webgl=TRUE---------------------------------------------------------
+library(trip)
+library(rangl)
+data(walrus818)
 
-## ----include=FALSE-------------------------------------------------------
-rgl::rgl.snapshot("vignettes/readme-figure/README-MultiPoints.png"); rgl::rgl.close()
+library(graticule)
+prj <-"+proj=laea +lon_0=0 +lat_0=90 +ellps=WGS84"
+gr <- graticule(lats = seq(40, 85, by = 5), ylim = c(35, 89.5), proj = prj)
+library(maptools)
+data(wrld_simpl)
+
+w <- spTransform(subset(wrld_simpl, coordinates(wrld_simpl)[,2] > -70),  prj)
+library(graticule)
+walrus <- spTransform(walrus818, prj)
+
+gr$color_ <- "black"
+rgl::par3d(windowRect = c(100, 100, 912 + 100, 912 +100))
+plot(rangl(gr))
+w$color_ <- sample(viridis::inferno(nrow(w)))
+plot(rangl(w), specular = "black")
+plot(rangl(walrus))
+um <- structure(c(0.934230506420135, 0.343760699033737, 0.0950899347662926, 
+                  0, -0.302941381931305, 0.905495941638947, -0.297159105539322, 
+                  0, -0.188255190849304, 0.24880850315094, 0.950081348419189, 0, 
+                  0, 0, 0, 1), .Dim = c(4L, 4L))
+par3d(userMatrix = um)
+##rgl::rgl.snapshot("vignettes/readme-figure/README-walrus.png"); rgl.close()
 
 
