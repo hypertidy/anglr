18bea94e0211539728b1dc9c529e409c5396cdad
Michael Sumner <mdsumner@gmail.com>
2017-09-12 19:32:05 +1000

    deal with XYZ/M for lines
    Former-commit-id: 319a0f743549b914322f77e77c210719e9807c47

--- R/rangl-lines.r
+++ R/rangl-lines.r
@@ -29,6 +29,12 @@
 #' #l1 <- rangl(as(a, "SpatialLinesDataFrame") )
 #' #plot(l1)
 #' #plot(globe(l1))
+#' 
+#' #data("flight_tracks", package = "silicate")
+#' #r <- rangl(flight_tracks)
+#' #plot(r)
+#' #rgl::aspect3d(1, 1, 0.001)
+#' #rgl::rglwidget()
 rangl <- function(x, ...) {
   UseMethod("rangl")
 }
@@ -37,45 +43,61 @@ line_mesh_map_table1 <- function(tabs) {
   tabs$v$countingIndex <- seq(nrow(tabs$v))
   nonuq <- dplyr::inner_join(tabs$bXv, tabs$v, "vertex_")
   
-  pl <- list(P = as.matrix(tabs$v[, c("x_", "y_")]),
-                        S = do.call(rbind, lapply(split(nonuq, nonuq$branch_),
-                                                  function(x) path2seg(x$countingIndex))))
+  #pl <- list(P = as.matrix(tabs$v[, c("x_", "y_")]),
+             S = do.call(rbind, lapply(split(nonuq, nonuq$branch_),
+                                       function(x) path2seg(x$countingIndex)))
+  tabs$v$countingIndex <- NULL
+  tabs$v$vertex_ <- silicate::sc_uid(nrow(tabs$v))
+  #tabs$v <- tibble::tibble(x_ = pl$P[,1], y_ = pl$P[,2], vertex_ = spbabel:::id_n(nrow(pl$P)))
   
-  tabs$v <- tibble::tibble(x_ = pl$P[,1], y_ = pl$P[,2], vertex_ = spbabel:::id_n(nrow(pl$P)))
   tabs$b <- tabs$bXv <- NULL
-  tabs$l <- tibble::tibble(segment_ = spbabel:::id_n(nrow(pl$S)), object_ = tabs$o$object_[1])
+  #tabs$l <- tibble::tibble(segment_ = spbabel:::id_n(nrow(pl$S)), object_ = tabs$o$object_[1])
+  tabs$l <- tibble::tibble(segment_ = spbabel:::id_n(nrow(S)), object_ = tabs$o$object_[1])
+  
   tabs$lXv <- tibble::tibble(segment_ = rep(tabs$l$segment_, each = 2), 
-                             vertex_ = tabs$v$vertex_[as.vector(t(pl$S))])
+                             vertex_ = tabs$v$vertex_[as.vector(t(S))])
   
   tabs
 }
 
 rangl_lines <- function(tabs, ...) {
-ll <- vector("list", nrow(tabs$o))
-for (i_obj in seq(nrow(tabs$o))) {
-  tabs_i <- tabs; tabs_i$o <- tabs_i$o[i_obj, ]
-  tabs_i <- semi_cascade(tabs_i)
-  tt_i <- line_mesh_map_table1(tabs_i)
-  ll[[i_obj]] <- tt_i
-}
-
-outlist <- vector("list", length(ll[[1]]))
-nms <- names(ll[[1]])
-names(outlist) <- nms
-for (i in seq_along(outlist)) {
-  outlist[[i]] <- dplyr::bind_rows(lapply(ll, "[[", nms[i]))
-}
-
-## renormalize the vertices
-allverts <- dplyr::inner_join(outlist$lXv, outlist$v, "vertex_")
-allverts$uvert <- as.integer(factor(paste(allverts$x_, allverts$y_, sep = "_")))
-allverts$vertex_ <- spbabel:::id_n(length(unique(allverts$uvert)))[allverts$uvert]
-outlist$lXv <- allverts[, c("segment_", "vertex_")]
-outlist$v <- dplyr::distinct_(allverts, "x_", "y_", "vertex_")
-## finally add longitude and latitude
-#outlist$meta <- tibble::tibble(proj = pr4, x = "x_", y = "y_", ctime = format(Sys.time(), tz = "UTC"))
-class(outlist) <- "linemesh"
-outlist
+  ll <- vector("list", nrow(tabs$o))
+  for (i_obj in seq(nrow(tabs$o))) {
+    tabs_i <- tabs; tabs_i$o <- tabs_i$o[i_obj, ]
+    tabs_i <- semi_cascade(tabs_i)
+    tt_i <- line_mesh_map_table1(tabs_i)
+    ll[[i_obj]] <- tt_i
+  }
+  outlist <- vector("list", length(ll[[1]]))
+  nms <- names(ll[[1]])
+  names(outlist) <- nms
+  for (i in seq_along(outlist)) {
+    outlist[[i]] <- dplyr::bind_rows(lapply(ll, "[[", nms[i]))
+  }
+  
+  ## renormalize the vertices
+  allverts <- dplyr::inner_join(outlist$lXv, outlist$v, "vertex_")
+  #browser()
+  allverts$uvert <- as.integer(factor(paste(allverts$x_, allverts$y_, sep = "_")))
+  allverts$vertex_ <- spbabel:::id_n(length(unique(allverts$uvert)))[allverts$uvert]
+  outlist$lXv <- allverts[, c("segment_", "vertex_")]
+  
+  qs <- rlang::quos(...)
+  if (length(qs) < 1L) {
+    geometry_dimension_label <- rev(class(x[[attr(x, "sf_column")]][[1]]))[3L]
+    qs <- rlang::syms(switch(geometry_dimension_label, 
+                             XY = c("x_", "y_", "vertex_"), 
+                             XYZ = c("x_", "y_", "z_", "vertex_"), 
+                             XYM = c("x_", "y_", "m_", "vertex_"), 
+                             XYZM = c("x_", "y_", "z_", "m_", "vertex_"))    )
+  }
+  print(names(allverts))
+  print(qs)
+  outlist$v <- dplyr::distinct(allverts, !!!qs)
+  ## finally add longitude and latitude
+  #outlist$meta <- tibble::tibble(proj = pr4, x = "x_", y = "y_", ctime = format(Sys.time(), tz = "UTC"))
+  class(outlist) <- "linemesh"
+  outlist
 }
 
 
