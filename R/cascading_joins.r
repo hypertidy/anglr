ca33766bbfc4c04a46f836e1d9bf33c0410ae7b8
Michael Sumner <mdsumner@gmail.com>
2017-09-14 18:00:30 +1000

    epic @@ long-running bug fix for semi_cascade

--- R/cascading_joins.r
+++ R/cascading_joins.r
@@ -14,14 +14,30 @@ semi_join_be_quiet_if_there_is_only_1 <- function(x, y, by = NULL, copy = FALSE,
 #' @importFrom dplyr semi_join
 #' @noRd
 semi_cascade <- function(x, ..., tables = c("o", "b", "bXv", "v")) {
-  first <- dplyr::filter(x[[tables[1]]], ...)
-  x[[1]] <- last <- first 
+  itab <- tables[1L]
+  first <- dplyr::filter(x[[itab]], ...)
+  x[[itab]] <- last <- first 
   tables <- tables[-1]
   for (itab in tables) {
     x[[itab]] <- last <- semi_join_be_quiet_if_there_is_only_1(x[[itab]], last)
   }
   x
 }
+
+# semi_cascade2 <- function(x, ..., tables = c("o", "b", "bXv", "v")) {
+#   #qs <- rlang::quos(...)
+#   first <- dplyr::filter(x[[tables[1]]], g == 1)
+#   x[[tables[1]]] <- last <- first 
+#   tables <- tables[-1]
+#   for (itab in tables) {
+#     print(names(last))
+#     print(itab)
+#     print(names(x[[itab]]))
+#     last <- semi_join_be_quiet_if_there_is_only_1(x[[itab]], last)
+#     x[[itab]] <- last
+#   }
+#   x
+# }
 
 # inner_cascade <- function(x, ..., tables = c("o", "b", "bXv", "v")) {
 #   first <- dplyr::filter(x[[tables[1]]], ...)
