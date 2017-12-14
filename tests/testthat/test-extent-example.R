9c1d85ebbca33ecb6cefcd2f55552b07cd843a1d
Michael Sumner <Michael.Sumner@aad.gov.au>
2017-12-14 16:59:02 +1100

    degenerate column test
    Former-commit-id: 628bb26bbd15f75348958ef38c0d8e0fddf6520b

---
+++ tests/testthat/test-extent-example.R
@@ -0,0 +1,13 @@
+context("test-extent-example.R")
+
+# Create an extent to triangulate
+library(sf)
+library(anglr)
+library(raster)
+my_extent <- st_as_sf(as(extent(c(153.185183093, 153.19443135, -27.705328446, -27.6967222119999)), 
+                         "SpatialPolygons"))
+anglr(my_extent, max_area = 0.008)
+
+test_that("dataframes with only geometry are handled", {
+  expect_that(anglr(my_extent, max_area = 0.008), is_a(""))
+})
