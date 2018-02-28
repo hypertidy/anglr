27555f660172bc035982b1ed065b44f59caebf87
Michael Sumner <mdsumner@gmail.com>
2018-03-01 03:05:58 +1100

    copy down general, plot3d.ARC
    Former-commit-id: 558d356091bd0fb8beb712ca9bd3d9f7692e82b8

--- R/anglr-vector-classes.R
+++ R/anglr-vector-classes.R
@@ -27,69 +27,6 @@ get_proj.PATH <- function(x, ...) {
 }
 
 
-#' @importFrom silicate PATH
-#' @importFrom rlang .data
-#' @export
-anglr.PATH <- function(x, z = NULL, ..., type = NULL, max_area = NULL) {
-  pr4 <- get_proj(x)
-  tabs <- x
-  tabs$meta <- tibble::tibble(proj = pr4, ctime = format(Sys.time(), tz = "UTC"))
-  thetype <- tabs[["path"]]$type[1]
- if (grepl("POLYGON", thetype, ignore.case = TRUE)) {
-    out <- pfft_polys(tabs, ..., max_area = max_area)
-  }
-  if (grepl("LINE", thetype, ignore.case = TRUE)) {
-    out <- anglr_lines(tabs)
-  }
-  
-  if (inherits(z, "BasicRaster")) {
-    ee <- raster::extract(z, as.matrix(out$v[, c("x_", "y_")]), method = "bilinear")
-    rproj <- get_proj(z)
-    vproj <- get_proj(out)
-    if (is.na(rproj) || is.na(vproj) || !vproj == rproj) {
-      warning(sprintf("model projection: %s\n\nz projection: %s", vproj, rproj))
-    }
-    if (all(is.na(ee))) warning("all raster values NA")
-    out$v$z_ <- ee
-    
-    z <- NULL
-  }
-  if (!is.null(z)) {
-    ## this line/poly thing is a mess, need to use SC as general case
-    if (grepl("POLYGON", thetype, ignore.case = TRUE)) {
-      v <- out$tXv %>% dplyr::inner_join(out$t) %>% 
-        dplyr::inner_join(out$o %>% dplyr::select(.data$object_, z), "object_") %>% 
-        dplyr::inner_join(out$v %>% dplyr::select(.data$vertex_, .data$x_, .data$y_)) %>% 
-        dplyr::select(.data$x_, .data$y_, z, .data$vertex_, .data$triangle_)
-      
-      names(v)[names(v) == z] <- "z_"
-      names(v)[names(v) == "vertex_"] <- "old"
-      
-      gp <- dplyr::group_indices(v,  .data$x_, .data$y_, .data$z_)
-      v$vertex_ <- silicate::sc_uid(length(unique(gp)))[gp]
-      tXv <- v %>% dplyr::select(.data$vertex_, .data$triangle_)
-      out$tXv <- tXv
-      v$old <- NULL
-    } else {
-    v <- out$lXv %>% dplyr::inner_join(out$l) %>% 
-      dplyr::inner_join(out$o %>% dplyr::select(.data$object_, z), "object_") %>% 
-      dplyr::inner_join(out$v %>% dplyr::select(.data$vertex_, .data$x_, .data$y_)) %>% 
-      dplyr::select(.data$x_, .data$y_, z, .data$vertex_, .data$segment_)
-    
-    names(v)[names(v) == z] <- "z_"
-    names(v)[names(v) == "vertex_"] <- "old"
-    
-    gp <- dplyr::group_indices(v,  .data$x_, .data$y_, .data$z_)
-    v$vertex_ <- silicate::sc_uid(length(unique(gp)))[gp]
-    lXv <- v %>% dplyr::select(.data$vertex_, .data$segment_)
-    out$lXv <- lXv
-    v$old <- NULL
-    }
-
-    out$v <- dplyr::distinct(v, .data$x_, .data$y_, .data$z_, .data$vertex_)
-  }
-  out
-}
 
 #' @rdname anglr
 #' @importFrom dplyr %>%  arrange distinct mutate
--- R/plot3d.R
+++ R/plot3d.R
@@ -7,6 +7,7 @@
 #' @return vertices and segment indices, invisibly
 #' @export
 #' @importFrom rgl plot3d
+#' @export plot3d
 #' @examples
 #' library(silicate)
 #' x <- SC(sf::read_sf(system.file("shape", "nc.shp", package = "sf")) %>% 
@@ -40,28 +41,84 @@ plot3d.SC <- function(x, ..., add = FALSE) {
 }
 #' @name plot3d
 #' @export
-plot3d.PATH <- function(x, ...) {
+plot3d.PATH <- function(x, ..., add = FALSE) {
   plot3d(silicate::SC(x), ...)
 }
 #' @name plot3d
 #' @export
-plot3d.sf <- function(x, ...) {
+plot3d.sf <- function(x, ..., add = FALSE) {
   plot3d(silicate::SC(x), ...)
 }
 #' @name plot3d
 #' @export
-plot3d.sfc <- function(x, ...) {
+plot3d.sfc <- function(x, ..., add = FALSE) {
   plot3d(silicate::SC(x), ...)
 }
 #' @name plot3d
 #' @export
-plot3d.Spatial <- function(x, ...) {
+plot3d.Spatial <- function(x, ..., add = FALSE) {
   plot3d(silicate::SC(x), ...)
 }
 #' @name plot3d
 #' @export
-plot3d.trip <- function(x, ...) {
+plot3d.trip <- function(x, ..., add = FALSE) {
   plot3d(silicate::SC(x), ...)
 }
 
 
+#' @name plot3d
+#' @export
+plot3d.ARC <- function(x, ..., add = FALSE) {
+  if (!"color_" %in% names(x$object)) {
+    x$object$color_ <- trimesh_cols(nrow(x$object))
+  }
+  haveZ <- "z_" %in% names(x$vertex)
+  if (haveZ) {
+    vb <- cbind(x$vertex$x_, x$vertex$y_, x$vertex$z_)
+  } else {
+    vb <- cbind(x$vertex$x_, x$vertex$y_, 0)
+  }
+  pindex <- dplyr::inner_join(x$object_link_arc, x$object[, c("object_", "color_")], "object_")
+  vindex <- dplyr::inner_join(x$arc_link_vertex, x$vertex, "vertex_")
+ v_id <- lapply(split(vindex, vindex$arc_), function(x) as.vector(path2seg(x$vertex_)))
+  if (!add) {
+    rgl::rgl.clear()
+  }
+ vindex <- match(unlist(v_id), x$vertex$vertex_)
+  rgl::segments3d(vb[vindex,], 
+                  col = rep(pindex$color_[match(x$arc_link_vertex$arc_, pindex$arc_)], each = 2))
+  if (getOption("rgl.useNULL") && interactive() && runif(1, 0, 1) > 0.96) {
+    message("rgl NULL device in use, do you need to run rgl::rglwidget()?")
+  }
+  ## TODO need an rgl level classed object
+  invisible(list(v = vb, is = vindex))
+  
+}
+#' @name plot3d
+#' @export
+plot3d.TRI <- function(x, ..., add = FALSE) {
+  if (!"color_" %in% names(x$object)) {
+    x$object$color_ <- trimesh_cols(nrow(x$object))
+  }
+  haveZ <- "z_" %in% names(x$vertex)
+  if (haveZ) {
+    vb <- cbind(x$vertex$x_, x$vertex$y_, x$vertex$z_)
+  } else {
+    vb <- cbind(x$vertex$x_, x$vertex$y_, 0)
+  }
+  pindex <- dplyr::inner_join(x$triangle, x$object[, c("object_", "color_")], "object_")
+  ##vindex <- dplyr::inner_join(x$triangle, x$vertex, "vertex_")
+vindex <- match(c(t(as.matrix(pindex[c(".vertex0", ".vertex1", ".vertex2")]))), x$vertex$vertex_)
+  #v_id <- lapply(split(vindex, vindex$arc_), function(x) as.vector(path2seg(x$vertex_)))
+  if (!add) {
+    rgl::rgl.clear()
+  }
+  #vindex <- match(unlist(v_id), x$vertex$vertex_)
+  rgl::triangles3d(vb[vindex,])
+  if (getOption("rgl.useNULL") && interactive() && runif(1, 0, 1) > 0.96) {
+    message("rgl NULL device in use, do you need to run rgl::rglwidget()?")
+  }
+  ## TODO need an rgl level classed object
+  invisible(list(v = vb, it = vindex))
+  
+}
