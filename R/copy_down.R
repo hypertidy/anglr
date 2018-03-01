df28235adb906aba374230c174a8a46b5b35926d
Michael Sumner <mdsumner@gmail.com>
2018-03-01 16:54:11 +1100

    auto_3d aspect ratio
    Former-commit-id: 5d5b6f39fa2540945fb86d74fef4c03e5d4ba93f

--- R/copy_down.R
+++ R/copy_down.R
@@ -42,10 +42,10 @@ copy_down.sc <- function(x, z = NULL, ..., .id = "z_") {
     p1 <- get_proj(x)
     p2 <- get_proj(z)
 
-    if (!anyNA(c(p1, p2)) && !(p1 == p2)) {
-      message("transforming model vertices to raster coordinate system")
-      xy <- proj4::ptransform(xy * pi/180, p1, p2, silent = TRUE)
-    }
+    #if (!anyNA(c(p1, p2)) && !(p1 == p2)) {
+    #  message("transforming model vertices to raster coordinate system")
+    #  xy <- proj4::ptransform(xy * pi/180, p1, p2, silent = TRUE)
+    #}
     z <- raster::extract(z[[1L]], xy, method = "bilinear")
   }
   if (is.null(z)) {
