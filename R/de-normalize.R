f963507bf08b6e1861ad8015841ea904d9c6f86a
Michael Sumner <mdsumner@gmail.com>
2018-03-03 14:55:33 +1100

    denormalize to Z for SC
    Former-commit-id: d7d5d1f94442fe1c7e4dba209b9ec8f307e8a7c6

--- R/de-normalize.R
+++ R/de-normalize.R
@@ -24,7 +24,23 @@ denorm_SEQ_addZ <- function(x, z, ..., .id = "z_") {
 
 ## PRIM works for SC, TRI
 denorm_PRIM_addZ <- function(x, z, ..., .id = "z_") {
-  link  <- vertex_link(x)  
+  coords  <- vertex_link(x)  
+  
+  if (inherits(x, "SC")) {
+    ## instances of primitives
+  priminst <- x$edge %>% inner_join(x$object_link_edge)
+  priminst[["edge_"]] <- silicate::sc_uid(priminst)
+  prim_long <- priminst %>% 
+    gather(edge_vertex, vertex, -object_, -edge_)
+  prim_long[[.id]] <- z[match(prim_long$object_, x$object$object_)]
+  prim_long <- prim_long %>% inner_join(x$vertex, c("vertex" = "vertex_"))
+  prim_long$vertex <- NULL
+  prim_long$vertex_ <- silicate::sc_uid(nrow(prim_long))
+  x$edge <- prim_long[c("edge_vertex", "vertex_", "edge_")] %>% tidyr::spread(edge_vertex, vertex_)
+  x$object_link_edge <- dplyr::distinct(prim_long, object_, edge_)
+  x$vertex <- dplyr::distinct(prim_long, x_, y_, z_, vertex_)
+  }
+x  
 }
 
 ## return name of think_link_vertex table
@@ -43,23 +59,23 @@ vertex_link.ARC <- function(x) {
 vertex_link.PATH <- function(x) {
   x[["path_link_vertex"]]
 } 
-## return name of think_link_vertex table
-vertex_link<- function(x, value,) {
-  UseMethod("vertex_link") 
+## return name of thing_link_vertex table
+'vertex_link<-' <- function(x, value) {
+  UseMethod("vertex_link<-") 
 }
-vertex_link<-.SC <- function(x, value,) {
+'vertex_link<-.SC<-' <- function(x, value) {
   x[["edge_link_vertex"]] <- value
   x
 } 
-vertex_link<-.TRI <- function(x, value,) {
+'vertex_link<-.TRI<-' <- function(x, value) {
   x[["triangle_link_vertex"]] <- value
   x
 } 
-vertex_link<-.ARC <- function(x, value,) {
+'vertex_link<-.ARC<-' <- function(x, value) {
   x[["arc_link_vertex"]] <- value
   x
 } 
-vertex_link<-.PATH <- function(x, value,) {
+'vertex_link<-.PATH<-' <- function(x, value) {
   x[["path_link_vertex"]] <- value
   x
 } 
