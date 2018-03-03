ee9194c9899c2ffc5d3b868cc99a6afb2e445657
Michael Sumner <mdsumner@gmail.com>
2018-03-03 17:16:01 +1100

    copy_down raster!
    Former-commit-id: 04f53ecc3795c6391fa0cb5f5e9a594314ca955b

--- R/copy_down.R
+++ R/copy_down.R
@@ -55,72 +55,104 @@ copy_down <- function(x, z = NULL, ..., .id = "z_") {
   stopifnot(is.character(.id))
   UseMethod("copy_down")
 }
-#' @name copy_down
-#' @export
-copy_down.sc <- function(x, z = NULL, ..., .id = "z_") {
-  
-  if (inherits(z, "BasicRaster")) {
+
+copy_downRaster<- function(x, z = NULL, ..., .id = "z_") {
     
     xy <- as.matrix(x$vertex[c("x_", "y_")])
     p1 <- get_proj(x)
     p2 <- get_proj(z)
     if (!anyNA(c(p1, p2)) && !(p1 == p2)) {
+      if (grepl("longlat", p1) && grepl("longlat", p2)) {
+        warning("both proj are different longlat, no transformation done")
+        
+      } else {
      message("transforming model vertices to raster coordinate system for copy down")
      xy <- proj4::ptransform(xy * pi/180, p1, p2, silent = TRUE)[, c(1, 2)]
-    }
+      }
+     }
     z <- raster::extract(z[[1L]], xy, method = "bilinear")
-  }
-  if (is.null(z)) {
-    ## nothing required
-  } else {
-    if (length(z) == 1L) x$vertex[[.id]] <- z
-    if (length(z) == nrow(x$object)) {
-      edge_instances <- x$edge %>% inner_join(x$object_link_edge)
-      edge_instances[["edge_"]] <- silicate::sc_uid(nrow(edge_instances))
-      edge_instances <- edge_instances %>% gather(edge_vertex, vertex, -object_, -edge_)
-      edge_instances[[.id]] <- z[match(edge_instances$object_, x$object$object_)]
-      edge_instances <- edge_instances %>% inner_join(x$vertex, c("vertex" = "vertex_"))
-      edge_instances[["vertex"]] <- group_indices(edge_instances, "x_", "y_", "z_")
-    }
-  } 
+
+        x$vertex[[.id]] <- z
+  
   
   x  
 }
+
+
+
 #' @name copy_down
 #' @export
 copy_down.SC <- function(x, z = NULL, ..., .id = "z_") {
-  dmap <- x$object[c(z, "object_")] %>% 
-    dplyr::inner_join(x$object_link_edge[c("edge_", "object_")], "object_") %>%
-    dplyr::select(z, edge_) %>% 
-    dplyr::inner_join(x$edge, "edge_")  %>% 
-    dplyr::select(z, vertex_) %>% dplyr::distinct(vertex_, .keep_all = TRUE)
-  x$vertex[[.id]] <- dmap[[z]][match(dmap$vertex_, x$vertex$vertex_)]
+  if (inherits(z, "BasicRaster")) {
+   return(copy_downRaster(x, z = z, ..., .id = .id) )
+  }
+  denorm_PRIM_addZ(x, z = z, ..., .id = .id)
+}
+
+#' @name copy_down
+#' @export
+copy_down.TRI <- function(x, z = NULL, ..., .id = "z_") {
+  if (inherits(z, "BasicRaster")) {
+    return(copy_downRaster(x, z = z, ..., .id = .id) )
+  }
   
+  denorm_PRIM_addZ(x, z = z, ..., .id = .id)
 }
 #' @name copy_down
 #' @export
 copy_down.PATH <- function(x, z = NULL, ..., .id = "z_") {
-  stopifnot(.id %in% names(x$object))
-  if (is.character(z)) {
-    ## we need the join_ramp
-    dmap <- x$object[c(z, "object_")] %>% 
-      dplyr::inner_join(x$path[c("path_", "object_")], "object_") %>%
-      dplyr::select(z, path_) %>% 
-      dplyr::inner_join(x$path_link_vertex, "path_")  %>% 
-      dplyr::select(z, vertex_) %>% dplyr::distinct(vertex_, .keep_all = TRUE)
-    x$vertex[[.id]] <- dmap[[z]][match(dmap$vertex_, x$vertex$vertex_)]
+  if (inherits(z, "BasicRaster")) {
+    return(copy_downRaster(x, z = z, ..., .id = .id) ) 
   }
-  x
+  
+  denorm_SEQ_addZ(x, z = z, ..., .id = .id)
 }
+
 #' @name copy_down
 #' @export
-copy_down.TRI <- function(x, z = NULL, ..., .id = "z_") {
-  stopifnot(.id %in% names(x$object))
-  if (is.character(z)) {
-    ## we need the join_ramp
-    dmap <- x$object[c(z, "object_")] %>% 
-      dplyr::inner_join(x$triangle)
-##    WIP this is way trickier because we have to split the vertices again
-    }
-  x
+copy_down.ARC <- function(x, z = NULL, ..., .id = "z_") {
+  if (inherits(z, "BasicRaster")) {
+    return(copy_downRaster(x, z = z, ..., .id = .id) )
+  }
+  
+  denorm_SEQ_addZ(x, z = z, ..., .id = .id)
 }
+
+#  #@name copy_down
+#  #@export
+# copy_down.SC <- function(x, z = NULL, ..., .id = "z_") {
+#   dmap <- x$object[c(z, "object_")] %>% 
+#     dplyr::inner_join(x$object_link_edge[c("edge_", "object_")], "object_") %>%
+#     dplyr::select(z, edge_) %>% 
+#     dplyr::inner_join(x$edge, "edge_")  %>% 
+#     dplyr::select(z, vertex_) %>% dplyr::distinct(vertex_, .keep_all = TRUE)
+#   x$vertex[[.id]] <- dmap[[z]][match(dmap$vertex_, x$vertex$vertex_)]
+#   
+# }
+#  #@name copy_down
+#  #@export
+# copy_down.PATH <- function(x, z = NULL, ..., .id = "z_") {
+#   stopifnot(.id %in% names(x$object))
+#   if (is.character(z)) {
+#     ## we need the join_ramp
+#     dmap <- x$object[c(z, "object_")] %>% 
+#       dplyr::inner_join(x$path[c("path_", "object_")], "object_") %>%
+#       dplyr::select(z, path_) %>% 
+#       dplyr::inner_join(x$path_link_vertex, "path_")  %>% 
+#       dplyr::select(z, vertex_) %>% dplyr::distinct(vertex_, .keep_all = TRUE)
+#     x$vertex[[.id]] <- dmap[[z]][match(dmap$vertex_, x$vertex$vertex_)]
+#   }
+#   x
+# }
+#  #@name copy_down
+#  #@export
+# copy_down.TRI <- function(x, z = NULL, ..., .id = "z_") {
+#   stopifnot(.id %in% names(x$object))
+#   if (is.character(z)) {
+#     ## we need the join_ramp
+#     dmap <- x$object[c(z, "object_")] %>% 
+#       dplyr::inner_join(x$triangle)
+# ##    WIP this is way trickier because we have to split the vertices again
+#     }
+#   x
+# }
