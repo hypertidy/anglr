7fd40419a93b0eb558ebccac94583c18f69918fc
Michael Sumner <Michael.Sumner@aad.gov.au>
2018-03-05 16:21:05 +1100

    fix plot3d.TRI
    Former-commit-id: 1df7c390340ace19d2a42e895147b4281780146f

--- R/DEL-model.R
+++ R/DEL-model.R
@@ -34,24 +34,119 @@
 DEL <- function(x, ..., max_area = NULL) {
   UseMethod("DEL")
 }
+#' @name DEL
 #' @export
-DEL.SC <- function(x, ...) {
-  v <- x$vertex
-  a <- match(x$edge$.vertex0, v$vertex_)
-  b <- match(x$edge$.vertex1, v$vertex_)
-  p <- RTriangle::pslg(as.matrix(dplyr::select(v, .data$x_, .data$y_)), 
-                       S = cbind(a, b))
-  t <- RTriangle::triangulate(p, ...)
-  ## need pfft to drop holes...
-  meta <- tibble(proj = get_proj(x), ctime = Sys.time())
+DEL.SC <- function(x, max_area = NULL, ...)  {
+ objs <- vector("list", nrow(x$object))
+  for (i in seq_along(objs)) {
+    x1 <- x
+    x1$object <- x1$object[i, ]
+    x1$object_link_edge <- x1$object_link_edge %>% dplyr::filter(object_ == x1$object$object_[1])
+    x1$edge <- x$edge[x$edge$edge_ %in% x1$object_link_edge$edge_, ]
+    ordered_verts <- t(apply(as.matrix(x1$edge[c(".vertex0", ".vertex1")]), 1, sort))
+    x1$vertex <- x$vertex[x$vertex$vertex_ %in% c(ordered_verts), ]
+    
+
+    dots <- list(...)
+    
+    dots[["a"]] <- max_area
+    dots[["x"]] <- x1
+    ## TRIANGULATE
+    RTri <- do.call(edge_RTriangle, dots)
+    objs[[i]] <- RTri
+  }
+
+
+  ## unique triangles
+ n_t <- unlist(lapply(objs, function(x) nrow(x$T)))
+ nT <- sum(n_t)
+  triangle <- tibble::tibble(triangle_ = silicate::sc_uid(nT))
   
-  structure(list(TRI = t$T, V = cbind(t$P, 0)), class = "TRI")
+  P <- do.call(rbind, lapply(objs, function(x) x$P))
+  vertex <- tibble::tibble(x_ = P[,1], y_ = P[,2], 
+                           vertex_ = silicate::sc_uid(nrow(P)))
+  TRIS <- lapply(objs, function(x) x$T)
+  count <- 0
+
+  for (i in seq_along(TRIS)) {
+    TRIStemp <- TRIS[[i]] + count
+    count <- count + max(TRIS[[i]])
+    TRIS[[i]] <- TRIStemp
+  }
+
+  TRIS <- do.call(rbind, TRIS)
+  triangle <- dplyr::mutate(triangle, .vertex0 = vertex$vertex_[TRIS[,1]],
+                            .vertex1 = vertex$vertex_[TRIS[,2]],
+                            .vertex2 = vertex$vertex_[TRIS[,3]],  
+                            object_ = rep(x$object$object_, n_t))
+ object_link_triangle <- dplyr::distinct(triangle[c("object_", "triangle_")])
+ triangle$visible <- TRUE
+ triangle$object_ <- NULL
+  tXv <- tibble::tibble(vertex_ = vertex[["vertex_"]][t(TRIS)], 
+                        triangle_ = rep(triangle[["triangle_"]], each = 3))
+  
+  meta <- tibble(proj = get_proj(x), ctime = Sys.time())
+  structure(list(object = x$object, 
+                 object_link_triangle = object_link_triangle,
+                 triangle = triangle, 
+                 vertex = vertex, 
+                 meta = meta), class = c("DEL", "TRI", "sc"))
+  
 }
 
+#' @name DEL
 #' @export
 DEL.default <- function(x, ...) {
   DEL(SC(x), ...)
 }
+
+## DEL for a PATH is a copy of pfft_polys that returns a DEL, TRI, sc
+## TRI for a PATH returns a TRI, sc (just decido triangles)
+
+#' @name DEL
+#' @export
+DEL.PATH <- function(x, max_area = NULL,  ...) {
+  dots <- list(...)
+  dots[["a"]] <- max_area
+  dots[["x"]] <- x
+  
+  ## TRIANGULATE with PATH-identity  
+  RTri <- do.call(edge_RTriangle, dots)
+  ## object/path_link_triangle (path_triangle_map)
+  ptm <- pfft::path_triangle_map(x, RTri)
+  
+  ## unique triangles
+  triangle <- tibble::tibble(triangle_ = silicate::sc_uid(nrow(RTri$T)))
+  
+  ## all triangle instances
+  ptm[["triangle_"]] <- triangle[["triangle_"]][ptm[["triangle_idx"]]]
+  ptm[["triangle_idx"]] <- NULL
+  
+  ## any triangle that occurs an even number of times in a path 
+  ## per object is part of a hole
+  ptm <- dplyr::inner_join(ptm, x[["path"]][c("path_", "object_")], "path_")
+  object_link_triangle <- ptm %>% dplyr::group_by(.data$object_, .data$triangle_) %>% 
+    dplyr::mutate(visible_ = !(n() %% 2 == 0)) %>%  ## see globalVariables declaration for "n"
+    dplyr::ungroup()  
+  vertex <- tibble::tibble(x_ = RTri$P[,1], y_ = RTri$P[,2], 
+                           vertex_ = silicate::sc_uid(nrow(RTri$P)))
+  
+  triangle <- dplyr::mutate(triangle, .vertex0 = vertex$vertex_[RTri$T[,1]],
+                            .vertex1 = vertex$vertex_[RTri$T[,2]],
+                            .vertex2 = vertex$vertex_[RTri$T[,3]])
+  
+  tXv <- tibble::tibble(vertex_ = vertex[["vertex_"]][t(RTri$T)], 
+                        triangle_ = rep(triangle[["triangle_"]], each = 3))
+  
+  meta <- tibble(proj = get_proj(x), ctime = Sys.time())
+  
+  structure(list(object = x$object, object_link_triangle = object_link_triangle, 
+                 triangle = triangle, 
+                 vertex = vertex, 
+                 meta = meta), class = c("DEL", "TRI", "sc"))
+  
+}
+
 ## from pfft
 edge_RTriangle <- function (x, ...) 
 {
@@ -61,60 +156,16 @@ edge_RTriangle <- function (x, ...)
                                                                                 x[["vertex"]][["vertex_"]]), ncol = 2, byrow = TRUE))
   RTriangle::triangulate(ps, ...)
 }
-## DEL for a PATH is a copy of pfft_polys that returns a DEL, TRI, sc
-## TRI for a PATH returns a TRI, sc (just decido triangles)
-DEL.PATH <- function(x, max_area = NULL,  ...) {
-    dots <- list(...)
-    dots[["a"]] <- max_area
-    dots[["x"]] <- x
-  
-    ## TRIANGULATE with PATH-identity  
-    RTri <- do.call(edge_RTriangle, dots)
-    ## object/path_link_triangle (path_triangle_map)
-    ptm <- pfft::path_triangle_map(x, RTri)
-    
-    ## unique triangles
-    triangle <- tibble::tibble(triangle_ = silicate::sc_uid(nrow(RTri$T)))
-
-    ## all triangle instances
-    ptm[["triangle_"]] <- triangle[["triangle_"]][ptm[["triangle_idx"]]]
-    ptm[["triangle_idx"]] <- NULL
-    
-    ## any triangle that occurs an even number of times in a path 
-    ## per object is part of a hole
-    ptm <- dplyr::inner_join(ptm, x[["path"]][c("path_", "object_")], "path_")
-    object_link_triangle <- ptm %>% dplyr::group_by(.data$object_, .data$triangle_) %>% 
-      dplyr::mutate(visible_ = !(n() %% 2 == 0)) %>%  ## see globalVariables declaration for "n"
-      dplyr::ungroup()  
-    vertex <- tibble::tibble(x_ = RTri$P[,1], y_ = RTri$P[,2], 
-                             vertex_ = silicate::sc_uid(nrow(RTri$P)))
-
-    triangle <- dplyr::mutate(triangle, .vertex0 = vertex$vertex_[RTri$T[,1]],
-                              .vertex1 = vertex$vertex_[RTri$T[,2]],
-                              .vertex2 = vertex$vertex_[RTri$T[,3]])
-    
-    tXv <- tibble::tibble(vertex_ = vertex[["vertex_"]][t(RTri$T)], 
-                          triangle_ = rep(triangle[["triangle_"]], each = 3))
-    
-    meta <- tibble(proj = get_proj(x), ctime = Sys.time())
-  
-    structure(list(object = x$object, object_link_triangle = object_link_triangle, 
-                   triangle = triangle, 
-                   vertex = vertex, 
-                   meta = meta), class = c("DEL", "TRI", "sc"))
-    
-  }
-
-
 #' @importFrom rgl plot3d
 #' @export
-plot3d.DEL <- function(x) {
+plot3d.DEL <- function(x, ...,  add = FALSE) {
   nms <- intersect(c("x_", "y_", "z_"), names(x$vertex))
   if (length(nms) < 3) z <- 0 else z <- NULL
   V <- cbind(as.matrix(x$vertex[nms]), z)
   tXv <- dplyr::inner_join(x$triangle, x$object_link_triangle)
   TT <- rbind(match(tXv$.vertex0, x$vertex$vertex_), 
               match(tXv$.vertex1, x$vertex$vertex_),
-                    match(tXv$.vertex2, x$vertex$vertex_))
+              match(tXv$.vertex2, x$vertex$vertex_))
+  if (!add) rgl::rgl.clear()
   rgl::rgl.triangles(t(V[TT, ]))
 }
