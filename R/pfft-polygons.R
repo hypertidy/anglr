a0f94bf8541517f38da9f9249c51b1b7733944a4
Michael Sumner <mdsumner@gmail.com>
2019-10-12 05:08:47 +0000

    fixes

--- R/pfft-polygons.R
+++ R/pfft-polygons.R
@@ -11,10 +11,10 @@ extents.default <- function(x) extents(silicate::PATH(x))
 extents.SC <- function(x) {
   x0 <- x[["edge"]] %>%
     dplyr::inner_join(x[["vertex"]], c(".vx0" = "vertex_"))  %>%
-    dplyr::transmute(x0 = x_, y0 = y_)
+    dplyr::transmute(x0 = .data$x_, y0 = .data$y_)
   x1 <- x[["edge"]] %>%
     dplyr::inner_join(x[["vertex"]], c(".vx1" = "vertex_"))  %>%
-    dplyr::transmute(x1 = x_, y1 = y_, edge_ = edge_)
+    dplyr::transmute(x1 = .data$x_, y1 = .data$y_, edge_ = .data$edge_)
   
   edges <- dplyr::bind_cols(x0, x1)
   tibble::tibble(xmn = pmin(edges$x0, edges$x1), xmx = pmax(edges$x0, edges$x1),
@@ -30,7 +30,8 @@ extents.PATH <- function(x) {
     dplyr::inner_join(x[["path_link_vertex"]], "path_") %>%
     dplyr::inner_join(x[["vertex"]], "vertex_") %>%
     dplyr::group_by(.data$path_) %>%
-    dplyr::summarize(xmn = min(.data$x_), xmx = max(.data$x_), ymn = min(.data$y_), ymx = max(.data$y_))
+    dplyr::summarize(xmn = min(.data$x_), xmx = max(.data$x_), 
+                     ymn = min(.data$y_), ymx = max(.data$y_))
 }
 
 
