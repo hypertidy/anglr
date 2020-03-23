b5dc089bae790ee4a1de19d7b8841a9df0cec655
Michael Sumner <mdsumner@gmail.com>
2020-03-24 09:46:43 +1100

    polygons as triangles

--- data-raw/mapdeck_examples.R
+++ data-raw/mapdeck_examples.R
@@ -2,7 +2,6 @@
 library(mapdeck)
 mapdeck() %>% add_mesh(data = melbourne_mesh)
 library(anglr)
-library(marmap)
 library(ceramic)
 cc <- cc_elevation(raster::extent(147.5, 147.6, -42.1, -42.0),
                    zoom = 9)
@@ -19,6 +18,20 @@ topo_quad <- as.mesh3d(raster::crop(quadmesh::etopo, raster::extent(100, 180, -5
 topo_triangle <- as.mesh3d(raster::crop(quadmesh::etopo, raster::extent(100, 180, -50, -20)),
                            triangles = TRUE)
 #rgl::clear3d();rgl::shade3d(topo_triangle);rgl::aspect3d(1, 1, 0.01);  rgl::rglwidget()
-
+library(sf)
+poly <- st_transform(subset(silicate::inlandwaters, Province %in% c("Victoria", "South Australia")),
+                     4326) ## reproj via anglr doesn't work atm because WKT2
+poly_triangle <- as.mesh3d(silicate::TRI0(poly))
+poly_triangle_dense <- as.mesh3d(
+  copy_down(anglr::DEL(poly, max_area =.005), quadmesh::etopo)
+)
+#rgl::clear3d();rgl::shade3d(poly_triangle_dense);rgl::aspect3d(1, 1, 0.3)
 saveRDS(list(topo_quad = topo_quad,
-             topo_triangle = topo_triangle), file = "data-raw/mesh3d_examples.rds", compress = "xz")
+             topo_triangle = topo_triangle,
+             poly_triangle = poly_triangle,
+             poly_triangle_dense = poly_triangle_dense),
+        file = "data-raw/mesh3d_examples.rds", compress = "xz")
+
+
+library(mapdeck)
+mapdeck() %>% add_mesh(data = poly_triangle)
