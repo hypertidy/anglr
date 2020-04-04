91d093f40ccf69dfa6e611a74dc927cc74e6ffb5
Michael Sumner <mdsumner@gmail.com>
2020-04-04 21:10:48 +1100

    fix failing tests (due to gebco change)

--- tests/testthat/test-sanity.R
+++ tests/testthat/test-sanity.R
@@ -21,11 +21,11 @@ test_that("sanity prevails 3", {
 })
 test_that("sanity prevails 4", {
   z <- copy_down(DEL(minimal_mesh), gebco)
-  expect_equivalent(z$vertex$z_, c(-4890, -4916.05, -4828.88, -4795.832, -4846.382, -4813.1744,
-                                   -4841.022, -4877.1168, -4849.578, -4846.94, -4862.78, -4875.4552,
-                                   -4772.29496, -4758.55736))
+  expect_equivalent(z$vertex$z_, c(-4928.25, -4889.75, -4814.5, -4782.75, -4845.25, -4822.09,
+                                   -4857.72, -5029.45, -4937.55, -4915.75, -4900.55, -4992.54, -4778.644,
+                                   -4787.672))
   expect_message(bb <- copy_down(SC(cont_tas), gebco), "transforming")
-  expect_true(all(bb$vertex$z_ > 150 & bb$vertex$z_ < 200))
+  expect_true(all(bb$vertex$z_ > 11 & bb$vertex$z_ < 20))
   expect_silent(copy_down(DEL(minimal_mesh), gebco))
   expect_silent(copy_down(TRI(minimal_mesh), gebco))
   expect_silent(copy_down(DEL(minimal_mesh), "a"))
