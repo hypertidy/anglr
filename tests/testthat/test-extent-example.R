3338272787fee64265b598dfcc83cc00673b34a8
Michael Sumner <mdsumner@gmail.com>
2020-04-06 21:28:19 +1000

    remove

--- tests/testthat/test-extent-example.R
+++ tests/testthat/test-extent-example.R
@@ -1,16 +1,17 @@
 context("test-extent-example.R")
 
 # Create an extent to triangulate
-library(sf)
 library(anglr)
 library(raster)
-my_extent <- st_as_sf(as(extent(c(153.185183093, 153.19443135, -27.705328446, -27.6967222119999)), 
-                         "SpatialPolygons"))
+library(sfheaders)
+my_extent <- sf_poly(data.frame(x = c(154, 155, 155, 154), y = c(-27, -27, -26, -27),
+                                polygon_id = 1, linestring_id = 1))
+
 test_that("setting max area makes more triangles", {
   ## no change
-  expect_that(nrow(DEL(my_extent, max_area = 0.008)$triangle), 
-              equals(nrow(DEL(my_extent, max_area = 0.0008)$triangle)))
-expect_true(nrow(DEL(my_extent, max_area = 0.000008)$triangle) >
+  expect_that(nrow(DEL(my_extent, max_area = 0.008)$triangle),
+              equals(96L))
+expect_true(nrow(DEL(my_extent, max_area = 0.00008)$triangle) >
              nrow(DEL(my_extent, max_area = 0.008)$triangle))
 })
 test_that("dataframes with only geometry are handled", {
