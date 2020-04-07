a041498ab9a6b355d23345884f0f4ee0505da16c
Michael Sumner <mdsumner@gmail.com>
2020-04-07 18:18:12 +1000

    stuffs

--- R/zzz.R
+++ R/zzz.R
@@ -6,8 +6,8 @@
 }
 .onLoad <- function(libname, pkgname) {
   ## there's no point doing this, because rgl is loaded first
-  use_rgl_null <- !screen_device()
-  if (use_rgl_null) Sys.setenv(RGL_USE_NULL=TRUE)
+  # use_rgl_null <- !screen_device()
+  # if (use_rgl_null) Sys.setenv(RGL_USE_NULL=TRUE)
 
   op <- getOption("anglr.max.triangles")
   if (is.null(op)) {
