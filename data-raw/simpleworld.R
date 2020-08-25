9e21fd35f82a04bece99de5c76316ce7348e8de9
Michael Sumner <mdsumner@gmail.com>
2020-08-26 08:30:35 +1000

    update sp crs and "comment"

--- data-raw/simpleworld.R
+++ data-raw/simpleworld.R
@@ -1,3 +1,5 @@
 simpleworld <- rgeos::gBuffer(rnaturalearth::countries110, width= 0, byid = TRUE)
 simpleworld <- simpleworld[c("sovereignt", "continent")]
+## #155
+simpleworld@proj4string <- sp::CRS("+proj=longlat +datum=WGS84")
 usethis::use_data(simpleworld)
