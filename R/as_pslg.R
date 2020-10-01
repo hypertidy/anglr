dd5cf70b08a0aecd14f4f402fa4f39f53e716bae
Michael Sumner <mdsumner@gmail.com>
2020-10-01 00:16:09 +0000

    create pslg via SC0

---
+++ R/as_pslg.R
@@ -0,0 +1,47 @@
+#' Planar Straight Line Graph
+#'
+#' Create a 'pslg' which is a mesh of segments used in the RTriangle package.
+#'
+#' We create the pslg from unique vertices x, y and their segments. In the context of
+#' triangulating the functions [DEL0()] and [DEL()] do more to ensure that input
+#' polygons have their holes culled out (or classified by invisible triangles.)
+#'
+#' @param x data model (understood by [SC0()])
+#'
+#' @return class pslg, from RTriangle
+#' @export
+#'
+#' @examples
+#' as_pslg(minimal_mesh)
+as_pslg <- function(x) {
+  UseMethod("as_pslg")
+}
+#' @name as_pslg
+#' @export
+as_pslg.default <- function(x) {
+  x <- silicate::SC0(x)
+  v <- silicate::sc_vertex(x)  ## we expecting only x_, y_
+  segs <- do.call(rbind, silicate::sc_object(x)$topology_)
+  RTriangle::pslg(P = as.matrix(v[c("x_", "y_")]), S = as.matrix(segs[c(".vx0", ".vx1")]))
+}
+
+#' #' @name as_pslg
+#' #' @export
+#' as_pslg.default <- function(x) {
+#'   if (inherits(x, "grouped_df")) {
+#'     ## use the groupings as linestring_id, polygon_id, sf_id?
+#'   }
+#'   ## try if sfheaders knows the columns
+#'   if ("multipolygon_id" %in% names(x)) {
+#'    sfx <-   sfheaders::sf_multipolygon(x)
+#'   } else {
+#'     if ("polygon_id" %in% names(x)) {
+#'       sfx <-   sfheaders::sf_polygon(x)
+#'    }
+#'
+#'   }
+#'
+#' ## last resort
+#'   sfx <- sfheaders::sf_polygon(x)
+#'   as.pslg(sfx)
+#' }
